03ccffc1585503a98102b7b90e41185115137d27
Allon Chauhan <chauhanallonashish@gmail.com>
2025-03-19 14:06:37 -0400

    groovy script fix

--- script.groovy
+++ script.groovy
@@ -15,7 +15,7 @@ def buildImage() {
 def incrementVersion() {
     echo "incrementing app version"
     sh 'mvn build-helper:parse-version versions:set \
-        n-DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalVersion} versions:commit'
+        -DnewVersion=\\\${parsedVersion.majorVersion}.\\\${parsedVersion.minorVersion}.\\\${parsedVersion.nextIncrementalVersion} versions:commit'
     def matcher = readFile('pom.xml') =~ '<version>(.+)</version>'
     def version = matcher[0][1]
     env.IMAGE_NAME = "$version-$BUILD_NUMBER"
